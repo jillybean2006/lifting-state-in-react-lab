d3147ac69a85c5926dc831a017018a83911391fb
jillian <kjillian2025@gmail.com>
2026-02-04 08:15:29 -0600

    burgerstack update

--- src/components/BurgerStack/BurgerStack.jsx
+++ src/components/BurgerStack/BurgerStack.jsx
@@ -1,7 +1,18 @@
 
 
-const BurgerStack = (props) => {
-  return <ul>// map through props.ingredients</ul>;
+const BurgerStack = ({ ingredients }) => {
+  return (
+    <ul>
+      {ingredients.map((ingredient, index) => (
+        <li 
+          key={index}
+          style={{ backgroundColor: ingredient.color }}
+        >
+          {ingredient.name}
+        </li>
+      ))} 
+    </ul>
+  );
 };
 
 export default BurgerStack;
